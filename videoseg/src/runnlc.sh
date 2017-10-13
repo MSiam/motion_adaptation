ad5b8a1bb30b7a5e6392edd9035c52df9b02d631
menna <mennatul@ualberta.ca>
2017-10-13 17:16:16 -0600

    added some experimentation with crude motion saliency

--- videoseg/src/runnlc.sh
+++ videoseg/src/runnlc.sh
@@ -1,6 +1,6 @@
-HOME_DIR="/home/eren/Data/DAVIS/"
-for d in $(ls $HOME_DIR"Motion/"); do
-    echo item: $d
-    python nlc_online.py -imdir $HOME_DIR"JPEGImages/480p/"$d"/" -out $HOME_DIR"/Motion/" -maxsp 400 -iters 100
-done
-#python nlc_online.py -imdir $HOME_DIR"JPEGImages/480p/bmx-trees/" -out $HOME_DIR"/Motion/" -maxsp 400 -iters 100
+HOME_DIR="/home/menna/Datasets/DAVIS_OUT/DAVIS_original/DAVIS/"
+#for d in $(ls $HOME_DIR"Motion/"); do
+#    echo item: $d
+#    python nlc_experiments.py -imdir $HOME_DIR"JPEGImages/480p/"$d"/" -out $HOME_DIR"/Motion/" -maxsp 400 -iters 100
+#done
+python nlc_experiments.py -imdir $HOME_DIR"JPEGImages/480p/bmx-trees/" -out $HOME_DIR"/Motion/" -maxsp 400 -iters 100
