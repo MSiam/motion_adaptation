9e76be4292e76dd02de64eafaec5891c7b5ec5f9
mennatullah siam <mennatul@ualberta.ca>
2018-01-21 07:41:37 -0700

    fixed some errors in gt and file gneration for fbms

--- datasets/Custom/convert_ppm.py
+++ datasets/Custom/convert_ppm.py
@@ -8,17 +8,25 @@ import numpy as np
 
 main_dir= sys.argv[1]
 for d in os.listdir(main_dir):
-    current_dir= main_dir+d+'/GroundTruth/'
+    current_dir= main_dir+d+'/'
     for f in os.listdir(current_dir):
         print(f)
-        if 'PROB' in f or 'pgm' in f or 'dat' in f or 'png' in f:
-            continue
+        if d[-2]=='0':
+            if 'PROB' in f or 'pgm' in f or 'dat' in f or 'png' in f:
+                continue
+            img= Image.open(current_dir+f)
+            img_arr= np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)[:,:,0]
+            mask= np.zeros_like(img_arr)
+            mask[img_arr==0]= 255
 
-        img= Image.open(current_dir+f)
-        img_arr= np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], 3)[:,:,0]
-        mask= np.zeros_like(img_arr)
-        mask[img_arr==0]= 255
-#        plt.imshow(mask); plt.show()
+        else:
+            if 'dat' in f:
+                continue
+            img= Image.open(current_dir+f)
+            img_arr= np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0])
+            mask= np.zeros_like(img_arr)
+            mask[img_arr>50]= 255
+
         misc.imsave(current_dir+f.split('.')[0]+'.png', mask)
 
 
--- datasets/Custom/gen_files.py
+++ datasets/Custom/gen_files.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import sys
+import pdb
 
 img_dir= sys.argv[1]+'JPEGImages/'
 mask_dir= sys.argv[1]+'Annotations/'
@@ -10,6 +11,9 @@ for d in os.listdir(mask_dir):
     current_dir= mask_dir+d+'/'
     for f in os.listdir(current_dir):
         if f.split('.')[1]=='png':
-            write_file.write('JPEGImages/'+d+'/'+f.split('_gt')[0]+'.jpg '+'Annotations/'+d+'/'+f+'\n')
+            if '_gt' in f:
+                write_file.write('JPEGImages/'+d+'/'+f.split('_gt')[0]+'.jpg '+'Annotations/'+d+'/'+f+'\n')
+            else:
+                write_file.write('JPEGImages/'+d+'/'+f.split('.')[0]+'.jpg '+'Annotations/'+d+'/'+f+'\n')
 
 write_file.close()
