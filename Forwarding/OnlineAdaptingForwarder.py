68a116b0eb91d87fe605b7265c8ae24a6d7c54b0
mennatullah siam <mennatul@ualberta.ca>
2018-07-29 09:48:37 -0600

    cleaned up online forwarding code

--- Forwarding/OnlineAdaptingForwarder.py
+++ Forwarding/OnlineAdaptingForwarder.py
@@ -26,18 +26,18 @@ class OnlineAdaptingForwarder(OneshotForwarder):
     self.use_negatives = self.config.bool("use_negatives", True)
     self.mot_dir= self.config.unicode("targets_path", "")
     self.neg_th = self.config.float("adapt_th", 0.8)
+    self.few_shot_samples = self.config.int("few_shot_samples", 1)
 
   def _oneshot_forward_video(self, video_idx, save_logits):
     with Timer():
-      # finetune on first frame
-      self._finetune(video_idx, n_finetune_steps=self.n_finetune_steps)
-
+      # Test Network Variables + Resize output to same shape of Labels
       network = self.engine.test_network
       targets = network.raw_labels
       ys = network.y_softmax
       ys = self._adjust_results_to_targets(ys, targets)
       data = self.val_data
 
+      # Process minibatch forward for first frame
       n, measures, ys_argmax_val, logits_val, targets_val = self._process_forward_minibatch(
         data, network, save_logits, self.save_oneshot, targets, ys, start_frame_idx=0)
       last_mask = targets_val[0]
@@ -46,20 +46,18 @@ class OnlineAdaptingForwarder(OneshotForwarder):
       n_frames = data.num_examples_per_epoch()
 
       measures_video = []
-      files_motion= []
 #      files_annotations= sorted(os.listdir('/home/nray1/ms/FBMS/Annotations/480p/'+data.video_tag(video_idx) ))
       dirs= sorted(os.listdir(self.mot_dir))
       for t in xrange(0, n_frames):
+
+          # Probability Map Function
           def get_posteriors():
               n_, _, _, logits_val_, _ = self._process_forward_minibatch(
                 data, network, save_logits=False, save_results=False, targets=targets, ys=ys, start_frame_idx=t)
               assert n_ == 1
               return logits_val_[0]
-          if t == 0:
-              adapt_flag = 1
-          else:
-              adapt_flag = 0
 
+          # Read adaptation target and postprocess it
           f= open(self.mot_dir+dirs[video_idx]+'/%05d.pickle'%t, 'rb')
 #          f= open(self.mot_dir+data.video_tag(video_idx)+'/'+files_annotations[t].split('.')[0]+'.pickle', 'rb')
           mask = pickle.load(f)[:,:,1]
@@ -68,7 +66,11 @@ class OnlineAdaptingForwarder(OneshotForwarder):
           last_mask[mask>self.neg_th]=1
           last_mask= np.expand_dims(last_mask, axis=2)
 
-          negatives = self._adapt(video_idx, t, last_mask, get_posteriors, adapt_flag)
+          # Start Network Adaptation Only on first frame
+          if t < self.few_shot_samples:
+              negatives = self._adapt(video_idx, t, last_mask, get_posteriors, adapt_flag=1)
+
+          # Compute IoU measures
           n, measures, ys_argmax_val, posteriors_val, targets_val = self._process_forward_minibatch(
               data, network, save_logits, self.save_oneshot, targets, ys, start_frame_idx=t)
           assert n == 1
@@ -77,13 +79,6 @@ class OnlineAdaptingForwarder(OneshotForwarder):
           print >> log.v5, "Motion Adapted frame", t, ":", measure, " factor ", float(ys_argmax_val.sum())/(854*480)
 
           measures_video.append(measure)
-          #last_mask = ys_argmax_val[0]
-
-      # prune negatives from last mask
-      # negatives are None if we think that the target is lost
-#      if negatives is not None and self.use_negatives:
-#        last_mask[negatives] = 0
-      #########
 
       measures_video[:-1] = measures_video[:-1]
       measures_video = average_measures(measures_video)
@@ -91,32 +86,39 @@ class OnlineAdaptingForwarder(OneshotForwarder):
 
   def _adapt(self, video_idx, frame_idx, last_mask, get_posteriors_fn, adapt_flag=0):
     """
-    adapt_flag (int): 0:do not adapt, 1:adapt with hard labels based on teacher, 2:adapt on hard labels from last mask
+    adapt_flag (int): 0:do not adapt, 1:adapt with hard labels based on teacher,
+                      2:adapt on hard labels from last mask, 3:adapt on soft labels
     """
+
+    # Perform Mask erosion to reduce effect of false positive
     eroded_mask = grey_erosion(last_mask, size=(self.erosion_size, self.erosion_size, 1))
+
+    # Compute distance transform
     dt = distance_transform_edt(numpy.logical_not(eroded_mask))
 
+    # Adaptation target initialize
     adaptation_target = numpy.zeros_like(last_mask)
     adaptation_target[:] = VOID_LABEL
 
+    # Retrieve current probability map to adapt with
     current_posteriors = get_posteriors_fn()
-    if adapt_flag==2:
+    if adapt_flag == 2:
         positives = current_posteriors[:, :, 1] > self.posterior_positive_threshold
-    else:
+    elif adapt_flag == 1:
         positives = last_mask==1
 
     if self.use_positives:
       adaptation_target[positives] = 1
 
+    # Threshold based on distance transform
     threshold = self.distance_negative_threshold
     negatives = dt > threshold
     if self.use_negatives:
       adaptation_target[negatives] = 0
 
     do_adaptation = eroded_mask.sum() > 0
-    if adapt_flag == 0:
-        do_adaptation = False
 
+    # Save adaptation targets for debugging
     if self.debug:
       adaptation_target_visualization = adaptation_target.copy()
       adaptation_target_visualization[adaptation_target == 1] = 128
@@ -129,6 +131,7 @@ class OnlineAdaptingForwarder(OneshotForwarder):
 
     self.train_data.set_video_idx(video_idx)
 
+    # Start Adapting based on number of adaptation_steps
     for idx in xrange(self.n_adaptation_steps):
       do_step = True
       #if idx % self.adaptation_interval == 0:
@@ -138,14 +141,7 @@ class OnlineAdaptingForwarder(OneshotForwarder):
         loss_scale = self.adaptation_loss_scale
         adaption_frame_idx = frame_idx
       else:
-      #  print >> log.v4, "skipping current frame adaptation, since the target seems to be lost"
         do_step = False
-      #else:
-        # mix in first frame to avoid drift
-        # (do this even if we think the target is lost, since then this can help to find back the target)
-      #  feed_dict = self.train_data.feed_dict_for_video_frame(frame_idx=0, with_annotations=True)
-      #  loss_scale = 1.0
-      #  adaption_frame_idx = 0
 
       if do_step:
         loss, _, n_imgs = self.trainer.train_step(epoch=idx, feed_dict=feed_dict, loss_scale=loss_scale,
