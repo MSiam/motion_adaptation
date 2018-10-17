8bb731756e7d17a33900a1a877c416ae79721db0
Mennatullah Siam <mennatul@ualberta.ca>
2018-10-17 11:46:00 -0600

    Documented the work

--- run.sh
+++ run.sh
@@ -1,25 +1,25 @@
 #USAGE : python main.py config_path video_id [ number of video or -1 to load all vids]
 
-#for i in `seq 0 32`;
-#do
-#    python main.py configs/fordsm_online $i
-#done
-#python main.py configs/fordsm_online 0
-
+## DAVIS
+### Adaptation
 #python main.py configs/DAVIS16_unsupervised -1
 #python main.py configs/DAVIS16_teach -1
+#python main.py configs/DAVIS16_teachcont -1
 
-#python crf/crf_davis.py DAVIS16_unsupervised /home/nray1/ms/DAVIS/
+### CRF Postprocessing
+#python crf/crf_davis.py DAVIS16_unsupervised /home/mennatul/Datasets/DAVIS/
 #python crf/crf_davis.py DAVIS16_online_cont /home/nray1/ms/DAVIS/
 #python crf/crf_davis.py DAVIS16_online /home/nray1/ms/DAVIS/
 
+## LORDS HRI
+### Scale or Rotation
 #python main.py configs/fordsm_unsupervised -1
-#python main.py configs/fordsm_teachcont -1
-#python main.py configs/DAVIS16_teachcont_refined -1
+#python main.py configs/fordsm_teach -1
+python main.py configs/fordsm_teachcont -1
+
+### Tasks on Bottles
 #python main.py configs/fordsm_tasks_teach 0
 #python main.py configs/fordsm_tasks_teachcont 0
-python main.py configs/fbms_teach -1
 
-#python crf/crf_davis.py FORDS_online /home/nray1/ms/FORDS_Scale/
-
-
+## FBMS
+#python main.py configs/fbms_teach -1
