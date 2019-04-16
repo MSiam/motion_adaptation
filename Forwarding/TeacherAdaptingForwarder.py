8570c4d75f2afc0908ba7b8a9d04fb2ffab2a5e2
Mennatullah Siam <mennatul@ualberta.ca>
2019-04-15 23:04:39 -0600

    tkinter inference phase

--- Forwarding/TeacherAdaptingForwarder.py
+++ Forwarding/TeacherAdaptingForwarder.py
@@ -11,40 +11,116 @@ import cv2
 VOID_LABEL = 255
 import pickle
 from PIL import Image
+from Tkinter import *
+import threading
+import PIL.Image, PIL.ImageTk
 
 class TeacherAdaptingForwarder(OneshotForwarder):
   def __init__(self, engine):
     super(TeacherAdaptingForwarder, self).__init__(engine)
+
     self.n_adaptation_steps = self.config.int("n_adaptation_steps", 12)
-    self.adaptation_interval = self.config.int("adaptation_interval", 4)
     self.adaptation_learning_rate = self.config.float("adaptation_learning_rate")
-    self.posterior_positive_threshold = self.config.float("posterior_positive_threshold", 0.97)
-    self.distance_negative_threshold = self.config.float("distance_negative_threshold", 150.0)
     self.adaptation_loss_scale = self.config.float("adaptation_loss_scale", 0.1)
-    self.debug = self.config.bool("adapt_debug", False)
-    self.erosion_size = self.config.int("adaptation_erosion_size", 20)
-    self.use_positives = self.config.bool("use_positives", True)
-    self.use_negatives = self.config.bool("use_negatives", True)
     self.mot_dir= self.config.unicode("targets_path", "")
-    self.neg_th = self.config.float("adapt_th", 0.8)
     self.few_shot_samples = self.config.int("few_shot_samples", 1)
     self.dataset = self.config.unicode("davis_data_dir", "")
+
     self.adapt_flag = 1
+    self.seqs = sorted(os.listdir(self.mot_dir))
+
+    self.cap = cv2.VideoCapture(int(self.config.cam_idx))
+    self.frame = None
+    self.overlay = None
+    self.flo_w = 512; self.flo_h = 384
+    self.panel_img = None
+
+    self.root = Tk()
+    labelfont = ('times', 15, 'bold')
+    lbl = Label(self.root, text='Classes Learned')
+    lbl.config(font=labelfont)
+    lbl.grid(row=0, column=1)
+
+    btns = self.create_buttons()
+    self.current_sq = -1
+
+    self.stopEvent = threading.Event()
+    thread = threading.Thread(target=self.videoLoop, args=())
+    thread.start()
+
+    self.root.wm_title("MotAdapt Inference Phase")
+    self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
+
+    self.segmentFlag = False
+    self.adaptFirst = False
+
+  def onClick(self, idx):
+    self.current_sq = idx
+    self.adaptFirst = True
+
+  def create_buttons(self):
+    labelfont = ('times', 15, 'bold')
+    idx = 0
+    btns = []
+    for sq in sorted(os.listdir(self.mot_dir)):
+        btns.append(Button(self.root, text=sq,
+                           command=lambda inst = self, idx = idx: inst.onClick(idx),
+                           width=20))
+
+        btns[-1].config(font=labelfont)
+        btns[-1].grid(row=idx, column=1)
+        idx += 1
+
+    self.nrows = idx
+
+  def set_panel(self, panel, photo, r, c):
+    if panel is None:
+        panel = Label(self.root, image=photo)
+        panel.image = photo
+        panel.grid(row=r, column=c, rowspan=self.nrows)
+    else:
+        panel.configure(image=photo)
+        panel.image = photo
+    return panel
+
+  def videoLoop(self):
+    while not self.stopEvent.is_set():
+        _, self.frame = self.cap.read()
+        if self.frame is not None:
+            self.frame = cv2.resize(self.frame,
+                                    (self.flo_w, self.flo_h))
+            if self.segmentFlag:
+                self.forward_(self.adaptFirst)
+                photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.overlay[:,:,::-1]))
+            else:
+                photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(self.frame[:,:,::-1]))
+
+            if self.adaptFirst:
+                self.forward_(self.adaptFirst)
+                self.adaptFirst = False
+                self.segmentFlag = True
+
+            self.panel_img = self.set_panel(self.panel_img, photo, 0, 0)
+
+  def onClose(self):
+    self.segmentFlag = False
+    self.stopEvent.set()
+    self.root.quit()
 
   def PIL2array(self, img):
     return np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], 4)
 
   def create_overlay(self, img, mask, colors):
-    im= Image.fromarray(np.uint8(img))
+    im= PIL.Image.fromarray(np.uint8(img))
     im= im.convert('RGBA')
 
     mask_color= np.zeros((mask.shape[0], mask.shape[1],3))
     mask_color[mask==colors[1],2]=255
 
-    overlay= Image.fromarray(np.uint8(mask_color))
+    overlay= PIL.Image.fromarray(np.uint8(mask_color))
     overlay= overlay.convert('RGBA')
 
-    im= Image.blend(im, overlay, 0.7)
+    im= PIL.Image.blend(im, overlay, 0.7)
     blended_arr= self.PIL2array(im)[:,:,:3]
     img2= img.copy()
     img2[mask!=colors[0],:]= blended_arr[mask!=colors[0],:]
@@ -57,12 +133,15 @@ class TeacherAdaptingForwarder(OneshotForwarder):
   def _oneshot_forward_video(self, video_idx, save_logits):
     with Timer():
       # Test Network Variables + Resize output to same shape of Labels
-      network = self.engine.test_network
-      targets = network.raw_labels
-      ys = network.y_softmax
-      ys = self._adjust_results_to_targets(ys, targets)
-      data = self.val_data
+      self.network = self.engine.test_network
+      self.targets = self.network.raw_labels
+      self.ys = self.network.y_softmax
+      self.ys = self._adjust_results_to_targets(self.ys, self.targets)
+      self.data = self.val_data
 
+      self.root.mainloop()
+
+  def forward_(self, first):
       # Probability Map Function
       def get_posteriors():
           n_, _, _, logits_val_, _ = self._process_forward_minibatch(
@@ -70,47 +149,33 @@ class TeacherAdaptingForwarder(OneshotForwarder):
           assert n_ == 1
           return logits_val_[0]
 
-      # Adapt the network based on number of few_shot_samples
-      files_annotations = sorted(os.listdir(self.mot_dir + 'sq1/'))
-      for t in range(self.few_shot_samples):
-          f= self.mot_dir + 'sq1/' + files_annotations[t]
-          mask = cv2.imread(f, 0)
-          last_mask= np.zeros((mask.shape[0], mask.shape[1]), dtype=np.uint8)
-          last_mask[mask==255] = 1
-          last_mask[mask==128] = VOID_LABEL
-          last_mask= np.expand_dims(last_mask, axis=2)
-          loss = self._adapt(0, t, last_mask, get_posteriors)
-          print('Adapting on frame ', t, 'with loss = ', loss)
+      if first:
+          files_annotations = sorted(os.listdir(self.mot_dir + \
+                                        self.seqs[self.current_sq] + '/'))
 
-      print("Finished Adaptation")
-      cap = cv2.VideoCapture(self.config.cam_idx)
-      flo_w = 512; flo_h = 384
-      segmentFlag = False
-      data.camera = True
-#      for t in range(self.few_shot_samples, data.num_examples_per_epoch()):
-      while True:
-          # Capture Camera Live Feed Input
-          _, frame = cap.read()
-          frame = cv2.resize(frame, (flo_w, flo_h))
+          for t in range(self.few_shot_samples):
+              f = self.mot_dir + self.seqs[self.current_sq] + \
+                      '/' + files_annotations[t]
 
-          # Apply segmentation with the adapted network
-          if segmentFlag:
-              data.current_frame = frame[:, :, ::-1]
+              mask = cv2.imread(f, 0)
+              last_mask = np.zeros((mask.shape[0], mask.shape[1]), dtype=np.uint8)
+              last_mask[mask==255] = 1
+              last_mask[mask==128] = VOID_LABEL
+              last_mask = np.expand_dims(last_mask, axis=2)
+              # ToDo: Modify in data video_idx to new one
+              loss = self._adapt(0, t, last_mask, get_posteriors)
+              print('Adapting on frame ', t, 'with loss = ', loss)
+
+          print("Finished Adaptation")
+      else:
+          self.data.camera = True
+          while True:
+              self.data.current_frame = self.frame[:, :, ::-1]
               _, _, ys_argmax_val, posteriors_val, _, _ = self._process_forward_minibatch(
-                  data, network, False, False, targets, ys, start_frame_idx=0)
+                  self.data, self.network, False, False, self.targets, self.ys, start_frame_idx=0)
+
+              self.overlay = self.visualize(self.frame, ys_argmax_val)
               print('segmenting current frame')
-
-              overlay = self.visualize(frame, ys_argmax_val)
-              cv2.imshow('Live Feed', overlay)
-          else:
-              cv2.imshow('Live Feed', frame)
-
-          ch = cv2.waitKey(10)%256
-          if ch == ord('q'):
-              break
-          elif ch == ord('s'):
-              segmentFlag = not segmentFlag
-
 
   def _adapt(self, video_idx, frame_idx, last_mask, get_posteriors_fn):
     """
